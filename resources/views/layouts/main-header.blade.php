7a71a36988c2330470cc111c5a957cbb568e46cf
VPS Admin <admin@school.local>
2026-06-14 13:16:26 +0000

    feat: Dark sidebar with white text + Arabic-only locale
    - Remove language switcher dropdown from header
    - Add dark sidebar theme: #1a1f2e background, white text
    - Force APP_LOCALE=ar, APP_FALLBACK_LOCALE=ar in .env
    - Sidebar icons: white 70% opacity, blue on hover
    - Submenu: darker #141824 background
    - Menu dividers: subtle #2a3045

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -34,24 +34,6 @@ header start-->
     <!-- top bar right -->
     <ul class="nav navbar-nav ml-auto">
 
-        <!-- Language Switcher -->
-        <div class="btn-group mb-1">
-            <button type="button" class="btn btn-light btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                @if (App::getLocale() == 'ar')
-                    العربية <img src="{{ URL::asset('assets/images/flags/EG.png') }}" alt="" style="margin-right: 6px;">
-                @else
-                    English <img src="{{ URL::asset('assets/images/flags/US.png') }}" alt="" style="margin-right: 6px;">
-                @endif
-            </button>
-            <div class="dropdown-menu">
-                @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-                    <a class="dropdown-item" rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
-                        {{ $properties['native'] }}
-                    </a>
-                @endforeach
-            </div>
-        </div>
-
         <!-- Fullscreen -->
         <li class="nav-item fullscreen">
             <a id="btnFullscreen" href="#" class="nav-link" title="ملء الشاشة"><i class="ti-fullscreen"></i></a>
