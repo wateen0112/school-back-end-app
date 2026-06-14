b5d71675616d23c7902fe9d4a358af768b74ed8b
VPS Admin <admin@school.local>
2026-06-14 13:10:01 +0000

    fix: Quote escaping in sidebar shell blade component

--- resources/views/components/sidebar/shell.blade.php
+++ resources/views/components/sidebar/shell.blade.php
@@ -1,5 +1,5 @@
 @props([
-    dashboardUrl => url(/dashboard),
+    'dashboardUrl' => url('/dashboard'),
 ])
 
 <div class="scrollbar side-menu-bg" style="overflow: scroll">
@@ -9,14 +9,14 @@
             <a href="{{ $dashboardUrl }}">
                 <div class="pull-left">
                     <i class="ti-home" style="color: #5f7cf7;"></i>
-                    <span class="right-nav-text">{{ trans(main_trans.Dashboard) }}</span>
+                    <span class="right-nav-text">{{ trans('main_trans.Dashboard') }}</span>
                 </div>
                 <div class="clearfix"></div>
             </a>
         </li>
 
         <!-- Section Divider -->
-        <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title">{{ trans(main_trans.Programname) }}</li>
+        <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title">{{ trans('main_trans.Programname') }}</li>
 
         {{ $slot }}
     </ul>
